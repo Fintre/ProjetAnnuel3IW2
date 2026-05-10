3acea6a055247256e78d58d220465f51bce3d128
KomaedaN <thomasmrt200@gmail.com>
2026-05-10 23:46:41 +0200

    feature(access): ajout d'un access dans les routes pour autoriser les accès ou non à un utilisateur connecté, un guest, ou au deux

--- www/Views/dashboard.php
+++ www/Views/dashboard.php
@@ -1,6 +1,8 @@
 <div>
     <h2>Bienvenue sur le dashboard</h2>
-    <a href="/logout">Se deconnecter</a>
+    <form method="POST" action="/logout">
+        <button type="submit">Se deconnecter</button>
+    </form>
 </div>
 
 <?php include("isActiveNav.php"); ?>
